b1ee937f130302ae3b74bd011d50209d94c43089
gaetbout <gaetant.ansotte@hotmail.com>
2022-12-06 15:14:13 +0100

    fix: fix prev push clippy

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -11,14 +11,14 @@ fn do_logic(input: &str, size: usize) -> Option<u32> {
     let mut idx: u32 = size as u32;
     input.as_bytes().windows(size).into_iter().any(|win| {
         idx += 1;
-        stack_valid(&win, size)
+        stack_valid(win, size)
     });
     Some(idx - 1)
 }
 
 fn stack_valid(stack: &[u8], size: usize) -> bool {
     let mut set = HashSet::new();
-    stack.into_iter().for_each(|c| {
+    stack.iter().for_each(|c| {
         set.insert(c);
     });
     set.len() == size

5bee54f47777686f1baa39f36581b7aed0de6a10
gaetbout <gaetant.ansotte@hotmail.com>
2022-12-06 15:39:14 +0100

    opti: last push

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -11,22 +11,9 @@ fn do_logic(input: &str, size: usize) -> Option<u32> {
     let mut idx: u32 = size as u32;
     input.as_bytes().windows(size).into_iter().any(|win| {
         idx += 1;
-        stack_valid(win)
+        (HashSet::<&u8>::from_iter(win.iter())).len() == size
     });
     Some(idx - 1)
-}
-
-fn stack_valid(stack: &[u8]) -> bool {
-    let mut set = HashSet::new();
-    let mut is_valid = true;
-    stack.iter().for_each(|c| {
-        if set.contains(c) {
-            is_valid = false;
-            return;
-        }
-        set.insert(c);
-    });
-    is_valid
 }
 
 fn main() {
@@ -47,7 +34,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let input = advent_of_code::read_file("examples", 6);
+        let input = advent_of_code::read_file("inputs", 6);
         assert_eq!(part_two(&input), Some(2564));
     }
 }
